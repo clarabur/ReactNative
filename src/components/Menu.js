9ce0289ded467b17b71a955a65465024f7ac836c
clarabur <cbur@udesa.edu.ar>
2021-11-08 17:21:49 -0300

    menu parte del login

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import {auth} from "../firebase/config"
 import Register from "../screens/Register";
+import Login from "../screens/Login";
+import Home from '../screens/Home'
 
+const Drawer = createDrawerNavigator();
 class Menu extends Component{
     constructor(){
         super()
         this.state={
             loggedIn: false,
-            user: ""
+            user: "",
+            errorMessage: '',
+            errorCode:'',
             
         }
     }
@@ -33,9 +40,47 @@ class Menu extends Component{
         })
         .catch(error => console.log(error))
     }
+
+    login(email, pass){
+        auth.signInWithEmailAndPassword(email, pass)
+        .then((response) => {
+           console.log (response)
+           this.setState({
+               loggedIn: true,
+               user: response.user
+           })
+        })
+        .catch (error => {
+          console.log (error)
+          this.setState({
+            errorMessage: error.message,
+            errorCode: error.code
+          })
+        })
+    }
+
+
+    logout(){
+        auth.signOut()
+        .then(()=> {
+            this.setState({
+                loggedIn: false,
+                user: '',
+            })
+        })
+    }  
     render(){
         return(
-            <Register register={(email, pass)=>this.register(email,pass)}/>
+            
+            <NavigationContainer>
+            
+                <Login login={(email, pass) => this.login(email, pass)} errorMessage={this.state.errorMessage} errorCode={this.state.errorCode}/>
+                 <Register register={(email, pass) => this.register(email, pass)} errorMessage={this.state.errorMessage} errorCode={this.state.errorCode} />
+              
+            </NavigationContainer> 
+
+
+           
         )
     }
 }
---
+++ src/screens/Home.js
@@ -0,0 +1,79 @@
+import React, {Component} from "react";
+import Register from "./register";
+import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import {auth, db} from '../firebase/config'
+
+
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+           posteos: [],
+        }
+}
+componentDidMount() {
+    db.collection('post').onSnapshot(
+        docs => {
+            let post = []
+            docs.forEach(doc => {
+                post.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            })
+            console.log(post)
+
+            this.setState({
+                posteos: post
+            })
+        }
+    )
+}
+render (){
+    return(
+        <View styles={styles.container}>
+<FlatList
+data={this.state.posteos}
+keyExtractor={post => post.id}
+renderItem={ ({item}) => <Post postData={item} />}
+/>
+
+
+        </View>
+    )
+}
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal: 10,
+    },
+    image: {
+        height:250,
+    },
+    touchable:{
+        backgroundColor: 'red',
+        borderRadius: 4,
+
+    },
+    formulario:{
+       
+       marginHorizontal: 10,
+       padding: 20,
+
+    },
+    field: {
+        backgroundColor:'pink',
+        borderColor: 'blue',
+        borderWidth:1,
+        borderStyle:'solid',
+        height: 20,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+
+    }
+     
+
+})
+export default Home
