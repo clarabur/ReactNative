79663e284afba8f89d2bd6225d0e72718234fedd
clarabur <cbur@udesa.edu.ar>
2021-11-15 10:19:12 -0300

    post ya se pueden borrar los posteos

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Modal, TextInput, FlatList} from 'react-native';
 import { db, auth } from '../firebase/config';
 import firebase from 'firebase'
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 class Post extends Component{
     constructor(props){
         super(props);
         this.state = {
            
-         
+            posteos:[],
+            showAlert : false,
            likes: 0,
            myLike: false
         }
@@ -20,7 +22,11 @@ class Post extends Component{
                 myLike: this.props.postData.data.likes.includes(auth.currentUser.email)
             })
         }
+       
+    
     }
+    
+
     likear(){
         db.collection("posts").doc(this.props.postData.id).update({
             likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
@@ -44,13 +50,70 @@ class Post extends Component{
         })
         .catch(error => console.log(error))
     }
+    showAlert(){
+        this.setState({
+            showAlert: true
+        })
+    }
+    hideAlert(){
+        this.setState({
+            showAlert: false
+        })
 
+    }
+    //borrar posteo
+    borrarPosteo(){
+        db.collection("posts").doc(this.props.postData.id).delete({
+            posteos: firebase.firestore.FieldValue.arrayUnion()
+        })
+        .then(()=>{
+            this.setState({
+                posteos: this.state.posteos.length
+            })
+        })
+        
+    
+    }
+  
 
    
 render(){
     console.log(this.props.postData)
     return(
         <View style={styles.contanier}>
+             {
+            this.props.postData.data.owner === this.props.user.email ?
+            <TouchableOpacity style={styles.borrar} onPress={()=> this.showAlert()}> 
+            <Text>Borrar posteo</Text>
+            </TouchableOpacity>
+        
+        //copiar para el mensaje de error
+        <AwesomeAlert
+        show={this.state.showAlert}
+        showProgress ={false}
+        title='Borrar posteo'
+        message='Desea borrar el posteo'
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showCancelButton={true}
+        showConfirmButton={true}
+        cancelText="No"
+        confirmText="Yes, borrarlo"
+        confirmButtonColor="#DD6B55"
+        onCancelPressed={() => {
+            this.hideAlert();
+        }}
+        onConfirmPressed={() => {
+            this.borrarPosteo();
+        }}
+        />
+
+
+      
+           :
+           <Text></Text>
+        }
+           
         <Text style={styles.input}>Comentario: {this.props.postData.data.texto}</Text>
         <Text style={styles.input}>Foto: {this.props.postData.data.foto}</Text>
         <Text style={styles.input}>user: {this.props.postData.data.owner} </Text>  
@@ -101,6 +164,16 @@ input:{
    
     padding: 5,
 },
+borrar: {    
+    color: '#fff',
+    padding: 5,
+    backgroundColor :'#dc3545',
+    alignSelf: 'flex-end',
+    borderRadius: 4,
+    paddingHorizontal: 4,
+    marginBottom: 10,    
+}
+
 
 })
 
